b80646b85f03c8a6405ed4c1f0602219473e42e6
Vasin W <vasinwr@gmail.com>
2016-07-25 00:09:18 +0100

    new start to include sass compass

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thaisocApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the thaisocApp
+ */
+angular.module('thaisocApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thaisocApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thaisocApp
+ */
+angular.module('thaisocApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
