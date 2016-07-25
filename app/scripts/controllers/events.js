5f23b844656ce7fb4aa0c70a7f5bf96ac0d77e3d
Vasin W <vasinwr@gmail.com>
2016-07-25 15:44:08 +0100

    added about us page, and populates dummy event page

---
+++ app/scripts/controllers/events.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thaisocApp.controller:EventsCtrl
+ * @description
+ * # EventsCtrl
+ * Controller of the thaisocApp
+ */
+angular.module('thaisocApp')
+  .controller('EventsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
